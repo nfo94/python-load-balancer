4bb897fc05b2febc0e298b64cc04b2234f24a995
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2024-04-22 10:48:06 +0100

    chore: adjust project

--- python_load_balancer/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-loadbalancer = Flask(__name__)
-
-
-@loadbalancer.route("/")
-def router():
-    return "Hello, World!"
--- tests/test_loadbalancer.py
+++ tests/test_loadbalancer.py
@@ -1,5 +1,6 @@
 import pytest
-from python_load_balancer import loadbalancer
+
+from python_load_balancer.app import loadbalancer
 
 
 @pytest.fixture
@@ -8,6 +9,17 @@ def client():
         yield client
 
 
-def test_hello(client):
-    response = client.get("/")
-    assert response.data == b"Hello, World!"
+def test_host_routing_mango(client):
+    result = client.get("/", headers={"Host": "mango.com"})
+    assert b"This is the mango application." in result.data
+
+
+def test_host_routing_apple(client):
+    result = client.get("/", headers={"Host": "www.apple.com"})
+    assert b"This is the apple application." in result.data
+
+
+def test_host_routing_notfound(client):
+    result = client.get("/", headers={"Host": "www.notmango.com"})
+    assert b"Not Found" in result.data
+    assert 404 == result.status_code
