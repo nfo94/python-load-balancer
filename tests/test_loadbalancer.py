f311f606ecd620c0fd0938a2496652ffef0ee245
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2024-04-23 11:05:49 +0100

    test: add pathing tests

--- tests/test_loadbalancer.py
+++ tests/test_loadbalancer.py
@@ -23,3 +23,19 @@ def test_host_routing_notfound(client):
     result = client.get("/", headers={"Host": "www.notmango.com"})
     assert b"Not Found" in result.data
     assert 404 == result.status_code
+
+
+def test_path_routing_mango(client):
+    result = client.get("/mango")
+    assert b"This is the mango application." in result.data
+
+
+def test_path_routing_apple(client):
+    result = client.get("/apple")
+    assert b"This is the apple application." in result.data
+
+
+def test_path_routing_notfound(client):
+    result = client.get("/notmango")
+    assert b"Not Found" in result.data
+    assert 404 == result.status_code
